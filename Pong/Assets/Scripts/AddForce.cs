3076997496653801627736aa8dc723a16718d436
tomatosoldier <haoxunico@gmail.com>
2015-08-26 16:46:28 -0400

    added movements of sticks

--- Pong/Assets/Scripts/AddForce.cs
+++ Pong/Assets/Scripts/AddForce.cs
@@ -6,10 +6,11 @@ public class AddForce : MonoBehaviour {
 	public int horiSpeed;
 	private Rigidbody rb;
 
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		rb.AddForce (horiSpeed, 0, 0);
+		rb.AddForce (horiSpeed, Random.Range(-100,100) , 0);
 	}
 	
 	// Update is called once per frame
